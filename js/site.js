140551898746616dd97c4b4649f684179c26b2ac
skhan125 <112320594+skhan125@users.noreply.github.com>
2023-03-04 22:42:20 -0600

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,53 +1,5 @@
-// Wait for the DOM to be fully loaded
-$(document).ready(function($) {
-
-  // Get form elements
-  var nameField = $('#nameField input');
-  var emailField = $('#emailField input');
-  var passwordField = $('#passwordField input');
-  var signUpBtn = $('#signupBtn');
-  var signInBtn = $('#signinBtn');
-  
-  // Check form fields and enable/disable buttons accordingly
-  function checkFormFields() {
-    if (nameField.val() !== '' && emailField.val() !== '' && passwordField.val() !== '') {
-      signUpBtn.prop('disabled', false);
-      signInBtn.removeClass('disable');
-    } else {
-      signUpBtn.prop('disabled', true);
-      signInBtn.addClass('disable');
-    }
-  }
- 
-  $('#signupBtn').click(function() {
-    $(this).addClass('active');
-    $('#signinBtn').removeClass('active');
-  });
-  $('#signinBtn').click(function() {
-    $(this).addClass('active');
-    $('#signupBtn').removeClass('active');
-  });
-
-  // On keyup, check form fields
-  nameField.on('keyup', checkFormFields);
-  emailField.on('keyup', checkFormFields);
-  passwordField.on('keyup', checkFormFields);
-  
-  // On click, submit form
-  signUpBtn.on('click', function() {
-    alert('Thank you for signing up!');
-  });
-  
-  // On click, show sign in form
-  signInBtn.on('click', function() {
-    alert('Showing sign in form');
-  });
-
-  $("#signupBtn").click(function() {
-    alert("You have successfully signed up!");
-  });
-  $("#signinBtn").click(function() {
-    alert("You have successfully signed in!");
-  });
-
+// jQuery 3.x-style ready event and locally scoped $
+jQuery(function($) {
+	$('html').removeClass('nojs');
+	$('html').addClass('hasjs');
 });
